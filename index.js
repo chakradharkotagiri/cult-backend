8f099e1c2eae72ebf3aae2905cb5fd9cab30e848
chakradharkotagiri <chakradharkotagiri111@gmail.com>
2025-08-17 00:11:40 +0530

    Made CORS and URL changes for deployment

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
@@ -6,13 +7,27 @@ const cors = require('cors');
 
 
 
+
 const app = express();
 
 // ✅ Connect to MongoDB
 connectDB();
+const allowedOrigins = [
+    process.env.FRONTEND_URL,        // e.g. prod URL
+    "http://localhost:5173"          // dev URL
+  ];
 
 // ✅ Middleware should come before routes
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true
+  }));
 app.use(express.json()); // For JSON body parsing
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
