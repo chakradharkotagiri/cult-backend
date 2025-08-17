1eb0cd3c1e1b5c6a4489278db0598606532687e4
chakradharkotagiri <chakradharkotagiri111@gmail.com>
2025-08-17 13:47:04 +0530

    Added few logs in the signup route for debugging

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { register, login, getCurrentUser } = require('../controllers/authController');
 const upload = require('../middlewares/multer');
-const  verifyToken  = require('../middlewares/auth'); // ✅ Make sure you have this
+const  verifyToken  = require('../middlewares/auth'); 
 
 router.post('/signup', upload.single('avatar'), register);
 router.post('/login', login);
 
-// ✅ New route to get current logged-in user
+//  New route to get current logged-in user
 router.get('/me', verifyToken, getCurrentUser);
 
 module.exports = router;
